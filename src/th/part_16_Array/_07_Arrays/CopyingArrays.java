604e53dccfbfe33fd368cda0fdd2a9095c83b03f
git <271643894@qq.com>
2017-05-31 11:05:03 +0800

    2017531

--- src/th/part_16_Array/_07_Arrays/CopyingArrays.java
+++ src/th/part_16_Array/_07_Arrays/CopyingArrays.java
@@ -6,13 +6,14 @@ import java.util.Arrays;
 public class CopyingArrays {
 	
 	public static void main(String[] args) {
-		int []i=new int[7];
+		int []i=new int[]{1,2,3,4,5,6,7};
 		int []j=new int[10];
-		Arrays.fill(i, 47);
+//		Arrays.fill(i, 47);
 		Arrays.fill(j, 99);
 		System.out.println("i="+Arrays.toString(i));
 		System.out.println("j="+Arrays.toString(j));
 		/**
+		 * shallow copy 潜复制，如果复制对象数组只是复制了引用--而不是对象本身的copy
 		 * java.lang.System.arraycopy(Object src, int srcPos, 
 		 * 							  Object dest,int destPos, int length)
 		 * src:源数组
@@ -21,7 +22,7 @@ public class CopyingArrays {
 		 * destPos:从目标数组什么位置开始复制的偏移量
 		 * length:需要复制的元素个数
 		 */
-		System.arraycopy(i, 0, j, 0, 2);
+		System.arraycopy(i, 0, j, j.length-i.length, i.length);
 		System.out.println("j="+Arrays.toString(j));
 		/*
 		 * for循环应用于boolean
