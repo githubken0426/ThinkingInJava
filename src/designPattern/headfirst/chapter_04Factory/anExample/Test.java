cb90dae36fd8f30fe2ccfcb2164335b8344db448
Administrator <Administrator@10.0.0.112>
2017-07-03 17:30:36 +0800

    2017-7-3 17:30

--- src/designPattern/headfirst/chapter_04Factory/anExample/Test.java
+++ src/designPattern/headfirst/chapter_04Factory/anExample/Test.java
@@ -5,6 +5,7 @@ package designPattern.headfirst.chapter_04Factory.anExample;
  * @author ken
  * 
  * @date 2017年6月27日 下午3:32:38
+ * 
  */
 public class Test {
 	public static void main(String[] args) {
