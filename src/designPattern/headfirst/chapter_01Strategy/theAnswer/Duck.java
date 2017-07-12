81b338b0602aa0bc71458b471079844ea0d4296c
Administrator <Administrator@10.0.0.112>
2017-07-12 16:17:11 +0800

    2017-7-12

--- src/designPattern/headfirst/chapter_01Strategy/theAnswer/Duck.java
+++ src/designPattern/headfirst/chapter_01Strategy/theAnswer/Duck.java
@@ -5,8 +5,9 @@ public abstract class Duck {
 	/**
 	 * 策略模式：
 	 *  定义了算法组，分别封装起来，算法之间可以相互替换(使用共同的接口，算法可以替换)。算法的变化独立于使用算法的客户；
-	 * 此处使用组合和委托
-	 * 行为的实现委托给具体的实现类
+	 * 组合(has a关系):每个鸭子都有一个FlyBehavior和有一个QuackBehavior行为，鸭子的行为不是继承来的，
+	 * 				  是通过FlyBehavior和QuackBehavior组合到鸭子中的。
+	 * 委托(代理):行为的实现委托给具体的实现.
 	 */
 	FlyBehavior fly;
 	QuackBehavior quack;
