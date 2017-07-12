d61291641a8950b9ca0a85f7149bbb71e2ddaa19
Administrator <Administrator@10.0.0.112>
2017-07-12 15:32:58 +0800

    2017-7-12

--- src/designPattern/headfirst/chapter_03Decorator/anExample/beverage/Espresso.java
+++ src/designPattern/headfirst/chapter_03Decorator/anExample/beverage/Espresso.java
@@ -1,5 +1,10 @@
 package designPattern.headfirst.chapter_03Decorator.anExample.beverage;
-
+/**
+ * 具体组件
+ * @author ken
+ * 
+ * @date 2017年7月12日 下午2:48:05
+ */
 public class Espresso extends Beverage {
 	public Espresso() {
 		description = "Espresso";
--- src/designPattern/headfirst/chapter_03Decorator/anExample/decorator/Whip.java
+++ src/designPattern/headfirst/chapter_03Decorator/anExample/decorator/Whip.java
@@ -1,7 +1,11 @@
 package designPattern.headfirst.chapter_03Decorator.anExample.decorator;
 
 import designPattern.headfirst.chapter_03Decorator.anExample.beverage.Beverage;
-
+/**
+ * 具体的装饰者
+ * 
+ * @author ken 2017-6-15 pm 03:08:04
+ */
 public class Whip extends CondimentDecorator {
 	Beverage beverage;
 
