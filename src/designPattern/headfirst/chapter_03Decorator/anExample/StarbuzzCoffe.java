4ea36b49313e45071b9dee387bedfb913f674271
Administrator <Administrator@10.0.0.112>
2017-07-14 11:39:58 +0800

    2017-7-14 EclipseOxygen

--- src/designPattern/headfirst/chapter_03Decorator/anExample/StarbuzzCoffe.java
+++ src/designPattern/headfirst/chapter_03Decorator/anExample/StarbuzzCoffe.java
@@ -12,14 +12,14 @@ public class StarbuzzCoffe {
 //		Beverage beverage=new Espresso();
 		Beverage beverage2=new HouseBlend();
 		beverage2=new Mocha(beverage2);
-		beverage2=new Mocha(beverage2);
+//		beverage2=new Mocha(beverage2);
 		beverage2=new Whip(beverage2);
 		System.out.println(beverage2.getDescription()+",$"+beverage2.cost()+"\n");
 		
-		Beverage beverage3=new Espresso();
+		/*Beverage beverage3=new Espresso();
 		beverage3=new Mocha(beverage3);
 		beverage3=new Soy(beverage3);
 		beverage3=new Whip(beverage3);
-		System.out.println(beverage3.getDescription()+",$"+beverage3.cost());
+		System.out.println(beverage3.getDescription()+",$"+beverage3.cost());*/
 	}
 }
