b071176ea68fb150ad95281eb7b4571805a09f2a
Administrator <Administrator@cm-PC>
2018-01-16 09:07:00 +0800

    2018-1-16

---
+++ src/th/part_07_encapsulation/chapter_07/_791/Insect.java
@@ -0,0 +1,16 @@
+package th.part_07_encapsulation.chapter_07._791;
+
+public class Insect {
+	private int i=9;
+	protected int j;
+	Insect(){
+		System.out.println("i="+i+",j="+j);
+		j=39;
+	}
+	private static int x1=print("static Insect.x1 initialized!");
+			
+	static int print(String str) {
+		System.out.println(str);
+		return 47;
+	}
+}
