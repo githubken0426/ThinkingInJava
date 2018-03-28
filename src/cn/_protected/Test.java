5af8635c922ab202173fd53d221de5ead74d1189
Administrator <Administrator@cm-PC>
2018-03-28 10:20:59 +0800

    2018-3-28

--- src/cn/_protected/Test.java
+++ src/cn/_protected/Test.java
@@ -6,5 +6,7 @@ import cn._protected._02.Son;
 public class Test {
 	public static void main(String[] args) {
 		Fatcher son=new Son();
+		//compile error
+		//son.id;
 	}
 }
--- src/cn/_protected/_02/Test.java
+++ src/cn/_protected/_02/Test.java
@@ -5,10 +5,12 @@ import cn._protected._02.Son;
 
 public class Test {
 	public static void main(String[] args) {
-		Fatcher son=new Son();
-		/* 
+		Fatcher son = new Son();
+		/*
 		 * compile error
-		 * System.out.println(son.id);
-		System.out.println(son.getId(100));*/
+		 * 
+		 * System.out.println(son.id); 
+		 * System.out.println(son.getId(100));
+		 */
 	}
 }
