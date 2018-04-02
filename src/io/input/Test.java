c6637b7b49c7015496cf6cbb6a298bea69d07748
Administrator <Administrator@cm-PC>
2018-04-02 17:49:46 +0800

    2018-4-2

---
+++ src/io/input/Test.java
@@ -0,0 +1,11 @@
+package io.input;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+		InputStream input=new BufferedInputStream(new FileInputStream(""));
+	}
+}
