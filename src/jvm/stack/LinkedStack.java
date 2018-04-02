09fce6e71d7a4d200484d43ab1513e5a2836cd75
Administrator <Administrator@cm-PC>
2018-04-02 11:25:16 +0800

    2018-4-2

--- src/jvm/stack/LinkedStack.java
+++ src/jvm/stack/LinkedStack.java
@@ -2,37 +2,41 @@ package jvm.stack;
 
 /**
  * 链表栈(1.存储结构)
- * @author Administrator
- * 2016-9-27 11:14:55
+ * 
+ * @author Administrator 2016-9-27 11:14:55
  *
  */
 public class LinkedStack<T> {
-	
-	private static class Node<U>{
-		 U item;
-		 Node<U> next;
-		 Node(){
-			item=null;next=null;
+
+	private static class Node<U> {
+		U item;
+		Node<U> next;
+
+		Node() {
+			item = null;
+			next = null;
 		}
-		 Node(U item,Node<U> next){
-			this.item=item;
-			this.next=next;
+
+		Node(U item, Node<U> next) {
+			this.item = item;
+			this.next = next;
 		}
-		boolean end(){
-			return item==null&& next==null;
+
+		boolean end() {
+			return item == null && next == null;
 		}
 	}
-	
-	Node<T> top=new Node<T>();
-	
-	public void push(T item){
-		top=new  Node<T>(item,top);
+
+	Node<T> top = new Node<T>();
+
+	public void push(T item) {
+		top = new Node<T>(item, top);
 	}
-	
-	public T pop(){
-		T result=top.item;
-		while(!top.end()){
-			top=top.next;
+
+	public T pop() {
+		T result = top.item;
+		while (!top.end()) {
+			top = top.next;
 		}
 		return result;
 	}
