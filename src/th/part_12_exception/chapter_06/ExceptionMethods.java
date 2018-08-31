ea2f20960c74d51ec41e298dc5c3930a5b4d1592
Administrator <Administrator@cm-PC>
2018-08-31 14:38:38 +0800

    2018-8-31

--- src/th/part_12_exception/chapter_06/ExceptionMethods.java
+++ src/th/part_12_exception/chapter_06/ExceptionMethods.java
@@ -2,6 +2,12 @@ package th.part_12_exception.chapter_06;
 
 public class ExceptionMethods {
 
+	/**
+	 * 继承类应该override(覆盖)getLocalizedMessage()
+	 * 此方法来提供一个针对地区方言的错误信息
+	 * 默认getMessage()、getLocalizedMessage()两者一致
+	 */
+	
 	public static void main(String[] args) {
 		try {
 			throw new Exception("My Exception");
