c933796bedcb6195759775cd4bbd3b71874d8d07
Administrator <Administrator@cm-PC>
2018-08-31 15:02:57 +0800

    2018-8-31

--- src/th/part_12_exception/chapter_06/_02Rethorw/ReThorwing.java
+++ src/th/part_12_exception/chapter_06/_02Rethorw/ReThorwing.java
@@ -1,6 +1,7 @@
 package th.part_12_exception.chapter_06._02Rethorw;
 
 public class ReThorwing {
+	
 	public static void f() throws Exception {
 		throw new Exception("Throw from f();");
 	}
@@ -13,13 +14,24 @@ public class ReThorwing {
 			throw e;
 		}
 	}
-	
+	/**
+	 * 重抛异常会把异常抛到上级异常处理，
+	 * try后续 catch子句将被忽略，异常对象的所有信息都会被保存。
+	 * @throws Exception
+	 * @throws 
+	 * @date 2018年8月31日 下午2:53:44
+	 */
 	public static void h() throws Exception {
 		try {
 			f();
 		} catch (Exception e) {
 			System.out.println("h():e.printStackTrace():");
 			e.printStackTrace(System.out);
+			/**
+			 * fillInStackTrace()方法返回一个Throwable对象
+			 * 抛出的异常点从fillInStackTrace()方法调用点开始。
+			 */
+			System.out.println("h():e.fillInStackTrace():");
 			throw (Exception)e.fillInStackTrace();
 		}
 	}
