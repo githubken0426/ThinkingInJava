ed9734b7a15263050fecab0e7d1bf880628cf500
Administrator <Administrator@cm-PC>
2018-08-31 14:34:19 +0800

    2018-8-31

--- src/th/part_12_exception/chapter_01/MyException2.java
+++ src/th/part_12_exception/chapter_01/MyException2.java
@@ -24,4 +24,15 @@ public class MyException2 extends Exception{
 	public String getMessage(){
 		return "Detail Message:"+super.getMessage();
 	}
+	
+	/**
+	 * 继承类应该override(覆盖)getLocalizedMessage()
+	 * 此方法来提供一个针对地区方言的错误信息
+	 * 默认getMessage()、getLocalizedMessage()两者一致
+	 */
+	@Override
+	public String getLocalizedMessage() {
+		return super.getLocalizedMessage();
+	}
+	
 }
