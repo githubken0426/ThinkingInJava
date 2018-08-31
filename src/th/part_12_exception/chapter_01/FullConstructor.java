06711a3a8d1f57f1f8c478a4f9c56e99ed195913
Administrator <Administrator@cm-PC>
2018-08-31 14:05:02 +0800

    2018-8-31

--- src/th/part_12_exception/chapter_01/FullConstructor.java
+++ src/th/part_12_exception/chapter_01/FullConstructor.java
@@ -6,11 +6,19 @@ public class FullConstructor {
 		System.out.println("throw MyException ");
 		throw new MyException();
 	}
-	
+	/**
+	 * System.err 不会重定向，异常信息更容易看到。
+	 * System.out 会被重定向
+	 * @param args
+	 * @throws 
+	 * @date 2018年8月31日 下午2:00:54
+	 */
 	public static void main(String[] args){
 		try {
 			FullConstructor.f();
 		} catch (MyException e) {
+			//信息会输出到标准错误流(System.err)
+			e.printStackTrace();
 			e.printStackTrace(System.out);
 		}
 	}
