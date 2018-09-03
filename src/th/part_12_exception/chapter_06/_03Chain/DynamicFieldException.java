f93662e25cb2953db315e139bc382c00cf5070c9
Administrator <Administrator@cm-PC>
2018-09-03 08:44:14 +0800

    2018-9-3

--- src/th/part_12_exception/chapter_06/_03Chain/DynamicFieldException.java
+++ src/th/part_12_exception/chapter_06/_03Chain/DynamicFieldException.java
@@ -3,7 +3,7 @@ package th.part_12_exception.chapter_06._03Chain;
 /**
  * 异常链：在捕获一个异常后抛出另一个异常，并且希望把原始异常信息保存下来，这就是异常链。
  * 
- * Throwable可以通过构造器接受cause参数来追踪原始异常。这个cause就异常原由，代表着原始异常，
+ * Throwable可以通过构造器接受cause参数来追踪原始异常。这个cause就是异常原由，代表着原始异常，
  * 即使在当前位置创建并抛出行的异常，也可以通过这个cause追踪到异常最初发生的位置。
  * 
  * 只有Error,Exception,RuntimeException提供了带cause参数的构造器，其他的所以异常就只有通过initCause()来设置cause了。
