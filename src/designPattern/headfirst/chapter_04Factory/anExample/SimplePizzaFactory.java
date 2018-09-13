3850eaca30ddfe65491f64e3037626de034d0473
Administrator <Administrator@cm-PC>
2018-09-13 14:27:16 +0800

    20180913

--- src/designPattern/headfirst/chapter_04Factory/anExample/SimplePizzaFactory.java
+++ src/designPattern/headfirst/chapter_04Factory/anExample/SimplePizzaFactory.java
@@ -12,6 +12,13 @@ import designPattern.headfirst.chapter_04Factory.factoryMethod.Pizza;
  * @date 2017年7月3日 下午5:44:04
  */
 public class SimplePizzaFactory {
+	/**
+	 * 也可以创建静态方法(静态工厂)
+	 * 优点:调用方法不用创建工厂对象
+	 * 缺点:无法通过继承来改变创建方法的行为
+	 * @param type
+	 * @date 2018年9月13日 下午2:25:45
+	 */
 	public Pizza createPizza(String type) {
 		if(StringUtils.isBlank(type))
 			throw new UnsupportedOperationException();
