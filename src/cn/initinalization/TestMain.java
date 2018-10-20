3904bf757c82d8ed7cc3a663adc79ee488f62b17
ken <ken@192.168.1.106>
2018-10-20 08:36:33 +0800

    2018-10-20

--- src/cn/initinalization/TestMain.java
+++ src/cn/initinalization/TestMain.java
@@ -3,6 +3,7 @@ package cn.initinalization;
 public class TestMain {
 	public static void main(String[] args) {
 		try {
+			// 会执行静态块代码(不会执行构造函数)
 			Class.forName("cn.initinalization.Gun");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
