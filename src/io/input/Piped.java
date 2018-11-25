767b50170c2a395355a4541c8f9fb37a9841b684
ken <ken@192.168.1.106>
2018-11-25 09:37:24 +0800

    20181125

---
+++ src/io/input/Piped.java
@@ -0,0 +1,16 @@
+package io.input;
+
+public class Piped {
+	/**
+	 * PipedInputStream类与PipedOutputStream类用于在应用程序中创建管道通信.
+	 * 一个PipedInputStream实例对象必须和一个PipedOutputStream实例对象进行连接而产生一个通信管道.
+	 * PipedOutputStream可以向管道中写入数据,PipedIntputStream可以读取PipedOutputStream向管道中写入的数据.
+	 * 
+	 * 这两个类主要用来完成线程之间的通信.
+	 * 一个线程的PipedInputStream对象能够从另外一个线程的PipedOutputStream对象中读取数据.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
